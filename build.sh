89721e40d5696c3b14ccef4566616a2344740ba8
Iku Iwasa <iku.iwasa@gmail.com>
2019-09-23 22:38:36 +0900

    Setup packer build for Fedora CoreOS

---
+++ build.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+set -e
+
+if [ ! -f id_rsa ]; then
+	curl -o id_rsa https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant
+fi
+
+packer build -var-file vars.json fedora-coreos.json
