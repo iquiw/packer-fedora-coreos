098508116cfa5f26ccb46b657060ab65db918d5e
Iku Iwasa <iku.iwasa@gmail.com>
2020-09-08 23:14:55 +0900

    Prepare script to build Fedora CoreOS box with static IP

---
+++ build-statcip.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+set -e
+
+case $# in
+4)	local_ip=$1
+	static_ip=$2
+	gateway=$3
+	dns_server=$4
+	;;
+*)	echo "usage: $0 LOCAL_IP STATIC_IP GATEWAY DNS_SERVER" 1>&2
+	exit 1
+esac
+
+(cat ignition.yml; sed -e "s,@STATIC_IP@,$static_ip,; s,@GATEWAY@,$gateway,; s,@DNS_SERVER@,$dns_server,;" staticip.tmpl ) |
+	fcct --pretty --strict > ignition-staticip.cfg
+
+hash=$(sha256sum ignition-staticip.cfg | awk '{ print $1 }')
+
+devd -A "$local_ip" . &
+devd_pid=$!
+
+if [ ! -f id_rsa ]; then
+	curl -o id_rsa https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant
+fi
+
+packer build -var ignition_url="http://$local_ip/ignition-staticip.cfg" -var ignition_hash="--ignition-hash sha256-$hash" -var-file vars.json fedora-coreos.json
+
+kill "$devd_pid"
