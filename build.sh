9e60c4a2bca46589bd0601a9bc242f1cf461512d
mikyll <righi.michy@gmail.com>
2025-01-27 12:15:14 +0100

    fix: ignition_hash in build.sh
    After [commit 3470c3a
    ](https://github.com/iquiw/packer-fedora-coreos/commit/3470c3ad1b1136cf45a4ca820201542be1729313), script build.sh doesn't work because variable ignition_hash is not set

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ if [ ! -f id_rsa ]; then
 	curl -o id_rsa https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant
 fi
 
-packer build -var-file vars.json fedora-coreos.pkr.hcl
+hash=$(sha256sum ignition-staticip.cfg | awk '{ print $1 }')
+
+packer build -var-file vars.json -var ignition_hash="sha256-$hash" fedora-coreos.pkr.hcl
